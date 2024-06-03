9b29029508a42a21a631c68cd22134392d64c1ea
Сергей Ефанов <msega5@mail.ru>
2024-06-03 16:43:53 +0300

    Add Dev branch

---
+++ Models/MessengerContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Proxies;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Reflection.Emit;
+
+namespace MessengerApplication.Models
+{
+    public class MessengerContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        //private string _connectingString;
+
+        public MessengerContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=gb;Integrated Security=True;TrustServerCertificate=True;Trusted_Connection=True;")
+                .UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("user_id");
+                entity.HasIndex(e => e.Email).IsUnique();
+
+                entity.ToTable("users");
+                entity.Property(e => e.Id).HasColumnName("id");
+                entity.Property(e => e.Email).HasColumnName("email");
+                entity.Property(e => e.FirstName).HasColumnName("first_name").HasMaxLength(255);
+                entity.Property(e => e.LastName).HasColumnName("last_name").HasMaxLength(255);
+                entity.Property(e => e.Registered).HasColumnName("registered");
+                entity.Property(e => e.Active).HasColumnName("active");
+                entity.Property(e => e.Password).HasColumnName("password");
+            });
+
+            //OnModelCreatingPartial(modelBuilder);
+        }
+    }
+   // partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace MessengerApplication.Models
+{
+    public class User 
+    {
+        public Guid Id { get; set; }
+        public string? Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime Registered {  get; set; } = DateTime.Now;
+        public bool Active { get; set; } = true;
+        public string? Password { get; set; } = null;        
+    }
+}

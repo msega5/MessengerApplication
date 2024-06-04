c7c028bf7d5acce7f9e88cf72d757d7e2ea99dcc
Сергей Ефанов <msega5@mail.ru>
2024-06-04 11:39:02 +0300

    ChatApp server client added

---
+++ ChatCommon/Abstractions/IMessageSourceClient.cs
@@ -0,0 +1,12 @@
+﻿using MessengerApplication.ChatCommon.Models;
+
+namespace MessengerApplication.ChatCommon.Abstractions
+{
+    public interface IMessageSourceClient<T>
+    {
+        Task SendAsync(NetMessage message, T ep);
+        NetMessage Receive(ref T ep);
+        T CreateEndpoint();
+        T GetServer();
+    }
+}
